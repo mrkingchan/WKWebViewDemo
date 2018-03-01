a91dc4d3dd84646203dbb3a3d74f2f3f8439f115
CherryIce <a13530836164@163.com>
2018-03-01 15:58:14 +0800

    Initial Commit

---
+++ WKWebViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WKWebViewDemo
+//
+//  Created by Macx on 2018/3/1.
+//  Copyright © 2018年 胡斌. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WKWebViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WKWebViewDemo
+//
+//  Created by Macx on 2018/3/1.
+//  Copyright © 2018年 胡斌. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
